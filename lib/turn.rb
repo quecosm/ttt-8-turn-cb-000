d69bd1d051576eb173557fd6f44bd21734172395
Queco SM <sersalazar.marioni@gmail.com>
2018-07-25 16:02:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,8 +20,7 @@ def position_taken? (board, index)
 end
 
 def valid_move? (board, index)
-  if index.between (0,8) && position_taken? == false 
-  end
+  index.between (0,8) && position_taken? == false 
 end
 
 def turn 
