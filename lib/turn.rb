9044c90f228ef521529826bde02b9e7db267f7f6
Queco SM <sersalazar.marioni@gmail.com>
2018-07-25 17:46:37 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def turn (board)
   puts "Please enter 1-9:" 
     input = gets.strip
     index = input_to_index (input)
-    if index.between?(0,8) && position_taken?(board, index) == false
+    if index.between?(0,8) && !position_taken?(board, index)
       move(board,index,"X")
     else
       turn(board)
