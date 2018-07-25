3fc5f4bf19e183d452eb066de5c17b179e7e27e5
Queco SM <sersalazar.marioni@gmail.com>
2018-07-25 15:58:17 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index (input)
+  input.to_i - 1
+end
+
+def move (array, index, current_player = "X")
+  array[index] = current_player
+end
+
+def valid_move?
+end
+
+def turn 
+end
