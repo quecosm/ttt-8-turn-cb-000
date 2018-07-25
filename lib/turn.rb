c128b850b7018fb8c780e984976cfd4b93603ff9
Queco SM <sersalazar.marioni@gmail.com>
2018-07-25 16:36:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def turn (board)
 puts "Please enter 1-9:" 
     input = gets.strip
     index = input_to_index (input)
-    index.between?(0,8) && !position_taken?(board, index)
-   
-    
+    until index.between?(0,8) && !position_taken?(board, index)
+    move
+  end
 end
